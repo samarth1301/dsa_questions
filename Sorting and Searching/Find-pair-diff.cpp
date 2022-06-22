57fd7a07b3ba5d61ff3ddcfb3ac58482c859220a
Sam <samarthsingh890.ss@gmail.com>
2022-06-22 10:28:46 +0530

    Update Find-pair-diff.cpp

--- Sorting and Searching/Find-pair-diff.cpp
+++ Sorting and Searching/Find-pair-diff.cpp
@@ -14,4 +14,33 @@ bool findPair(int arr[], int size, int n){
         hmap[el]++;
     }
     return false;
+     //method-2
+    //sort array
+    sort(arr,arr+size);
+    int i=0,j=1;
+    //in case n==0, look for duplicate elements
+    if(n==0){
+        for(int i=1;i<size;i++){
+            if(arr[i-1]==arr[i]){
+                return true;
+            }
+        }
+        return false;
+    }
+    //else traverse through the array, in case diff found, return true,
+    //else , if diff is greater than n, increment i to lower it down
+    //if diff is lesser than n, increment j to increase diff
+    while(i<size-1 && j<size){
+        
+        if(arr[j]-arr[i]==n){
+            return true;
+        }
+        if((arr[j]-arr[i])>n){
+            i++;
+        }
+        else{
+            j++;
+        }
+    }
+    return false;
 }
