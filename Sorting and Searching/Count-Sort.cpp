82aff008b6b231e425bc10710172c38e890969fa
Sam <samarthsingh890.ss@gmail.com>
2022-06-23 17:40:54 +0530

    Create Count-Sort.cpp

---
+++ Sorting and Searching/Count-Sort.cpp
@@ -0,0 +1,45 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void countSort(vector<int> &arr,int n){
+    int min= *min_element(arr.begin(),arr.end());
+    int max = *max_element(arr.begin(),arr.end());
+    int range = max-min+1;
+    vector<int> hmap(range,0);
+    for(int i=0;i<n;i++){
+        hmap[arr[i]-min]++;
+    }
+    for(int i=1;i<range;i++){
+        hmap[i] += hmap[i-1];
+    }
+    vector<int> res(n,0);
+    for(int i=0;i<n;i++){
+        res[--hmap[arr[i]-min]] = arr[i];
+    }
+    for(int i=0;i<n;i++){
+        arr[i]=res[i];
+    }
+}
+
+
+// Driver  code
+int main()
+{
+    vector<int> arr = {1,3,2,6,11,15,4,-1};
+ 
+    countSort(arr,8);
+ 
+    cout << "Sorted character array is " << endl;
+    for(int i=0;i<8;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
